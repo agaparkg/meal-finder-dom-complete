6b665a9f992e1855323ac727d9779a692dcb2c6b
Azamat Gaparov <agaparovkg@gmail.com>
2024-04-08 18:02:29 -0700

    New changes

--- script.js
+++ script.js
@@ -24,7 +24,44 @@ function getRandomMeal() {
     .catch((err) => console.log(err));
 }
 
-function addMealToDOM({ strMeal, strMealThumb, strCategory, strInstructions }) {
+function addMealToDOM(meal) {
+  const { strMeal, strMealThumb, strCategory, strArea, strInstructions } = meal;
+
+  let ingredients = "";
+  //   `
+  //           <li>Egg Plants - 1 lb</li>
+  //           <li>Sugar - 2 tbs</li>
+  //           <li>Salt - 1 tsp</li>
+  //           <li>Pepper - 1 tsp</li>
+  //           <li>Garlic - 1 whole</li>
+  //           <li>Olive Oil - 3 tbs</li>
+  //           <li>Ground Pork - 4 oz</li>
+  //           <li>Rice Vinegar - 3 tbs</li>
+  //           <li>Soy Sauce - 2 tbs</li>
+  //           <li>Bay Leaf - 2</li>
+  //       `;
+  for (let i = 1; i <= 20; i++) {
+    if (meal[`strIngredient${i}`]) {
+      const stringIng = "strIngredient" + i;
+      const stringMea = "strMeasure" + i;
+
+      ingredients =
+        ingredients + `<li>${meal[stringIng]} - ${meal[stringMea]}</li>`;
+      // `<li>${meal[`strIngredient${i}`]} - ${meal[`strMeasure${i}`]}</li>`;
+      //   console.log(ingredients);
+      //   index = 0
+      //   ingredients = "" + <li>Egg Plants - 1 lb</li>;
+
+      //   index = 1
+      // ingredients = '<li>Egg Plants - 1 lb</li>' + '<li>Egg Plants - 1 lb</li>';
+
+      //   index = 2
+      // ingredients = '<li>Egg Plants - 1 lb</li><li>Egg Plants - 1 lb</li>' + '<li>Egg Plants - 1 lb</li>';
+    }
+  }
+
+  console.log(ingredients);
+
   single_mealEl.innerHTML = `
         <div class="single-meal">
           <h1>${strMeal}</h1>
@@ -33,23 +70,14 @@ function addMealToDOM({ strMeal, strMealThumb, strCategory, strInstructions }) {
             alt="${strMeal}"
           />
           <div class="single-meal-info">
-            <p>Vegetarian</p>
-            <p>Filipino</p>
+            <p>${strCategory ? `${strCategory}` : ""}</p>
+            <p>${strArea ? `${strArea}` : ""}</p>
           </div>
           <div class="main">
             <p>${strInstructions}</p>
             <h2>Ingredients</h2>
             <ul>
-              <li>Egg Plants - 1 lb</li>
-              <li>Sugar - 2 tbs</li>
-              <li>Salt - 1 tsp</li>
-              <li>Pepper - 1 tsp</li>
-              <li>Garlic - 1 whole</li>
-              <li>Olive Oil - 3 tbs</li>
-              <li>Ground Pork - 4 oz</li>
-              <li>Rice Vinegar - 3 tbs</li>
-              <li>Soy Sauce - 2 tbs</li>
-              <li>Bay Leaf - 2</li>
+              ${ingredients}
             </ul>
           </div>
         </div>
